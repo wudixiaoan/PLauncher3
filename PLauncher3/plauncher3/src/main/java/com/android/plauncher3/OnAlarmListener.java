adc24cd0c9e36971b326ec23eae21601b092b4f8
wudixiaoan <pa_more@163.com>
2017-04-18 17:38:50 +0800

    commit PLauncher3-master

---
+++ PLauncher3/plauncher3/src/main/java/com/android/plauncher3/OnAlarmListener.java
@@ -0,0 +1,5 @@
+package com.android.plauncher3;
+
+public interface OnAlarmListener {
+    public void onAlarm(Alarm alarm);
+}
---
+++ PLauncher3/plauncher3/src/main/java/com/android/plauncher3/ToggleWeightWatcher.java
@@ -0,0 +1,7 @@
+package com.android.plauncher3;
+
+import android.app.Activity;
+
+public class ToggleWeightWatcher extends Activity {
+
+}
